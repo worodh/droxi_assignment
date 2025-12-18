5e0e35d64a48456eda9649998e1b82123cfd456a
worodhaiadry <worod.h@gmail.com>
2025-12-18 11:21:10 +0200

    update reqs and change ui test to soft assertion

--- test_trello_board.py
+++ test_trello_board.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_check import check
 
 def test_urgent_cards(trello_ui_client):
     urgent_cards = trello_ui_client.get_urgent_cards()
@@ -11,7 +12,7 @@ def test_urgent_cards(trello_ui_client):
 def test_specific_card_ui(trello_ui_client):
     card = trello_ui_client.get_card_by_name("summarize the meeting")
     card_data = trello_ui_client.get_card_data(card)
-    assert card_data['title'] == "summarize the meeting", "Card title does not match expected value"
-    assert card_data['description'] == "For all of us\nPlease do so", "Card description does not match expected value"
-    assert 'New' in card_data['labels'], "Card does not have 'New' label"
-    assert card_data['list_name'] == 'To Do', "Card is not in the expected list 'To Do'"
+    check.equal(card_data['title'], "summarize the meeting", "Card title does not match expected value")
+    check.equal(card_data['description'], "For all of us\nPlease do so", "Card description does not match expected value")
+    check.is_in('New', card_data['labels'], "Card does not have 'New' label")
+    check.equal(card_data['list_name'], 'To Do', "Card is not in the expected list 'To Do'")
